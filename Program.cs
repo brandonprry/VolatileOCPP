02669091bbd298c983fd7e1cc300141c08a174d5
Brandon Perry <bperry@Brandons-MBP.attlocal.net>
2024-10-26 15:57:44 -0500

    code and schemas

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using WebSocketSharp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using (var ws = new WebSocket ("ws://Brandons-MBP.attlocal.net:8080")) {
+        ws.OnMessage += (sender, e) =>
+                          Console.WriteLine ("Laputa says: " + e.Data);
+
+        ws.Connect ();
+        ws.Send ("henlo");
+        Console.ReadKey (true);    
+        }
+    }
+}
