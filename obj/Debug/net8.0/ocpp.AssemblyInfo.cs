889138672460d4ee0243c6ee7eeb37597dcd1a46
Brandon Perry <bperry@Brandons-MBP.attlocal.net>
2024-10-28 16:10:15 -0500

    new scenario

--- obj/Debug/net8.0/ocpp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ocpp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ocpp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a599ae750825493ea9c90a014c0b7af80e47a65a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f3e524f692b59bbacf9bcc6d7533e9ccd2bc5da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ocpp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ocpp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
