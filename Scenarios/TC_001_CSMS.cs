161f40ccf451f96f6f904e2425c6c1c28ac63239
Brandon Perry <bperry@Brandons-MBP.attlocal.net>
2024-10-27 21:51:16 -0500

    corrected scenarios

--- Scenarios/TC_001_CSMS.cs
+++ Scenarios/TC_001_CSMS.cs
@@ -8,48 +8,48 @@ public class TC_001_CSMS : IScenario
 {
     public bool RunScenario(string url, string protocol)
     {
-        using (var ws = new WebSocket(url, protocol))
-        {
-            ws.Connect();
+        using var ws = new WebSocket(url, protocol);
+        ws.Connect();
 
-            int i = 1;
-            ws.OnMessage += (sender, e) =>
+        int i = 1;
+        ws.OnMessage += (sender, e) =>
+       {
+           JArray a = JArray.Parse(e.Data);
+           JObject? j = a[2] as JObject;
+
+           if (j == null)
+               return;
+
+           if (i == 1)
            {
+               i++;
+               if (!Utility.ValidateJSON(j, File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/BootNotificationResponse.json")))
+                   throw new Exception("Invalid response");
 
-               JArray a = JArray.Parse(e.Data);
-               JObject j = a[2] as JObject;
-               if (i == 1)
-               {
-                     i++;
-                   if (!Utility.ValidateJSON(j, System.IO.File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/BootNotificationResponse.json")))
-                       throw new Exception("Invalid response");
+               if (j["status"] == null ||
+                   j["status"].Value<string>() != "Accepted")
+                   throw new Exception("Invalid response");
+           }
+           else if (i == 2)
+           {
+               i++;
+               if (!Utility.ValidateJSON(j, File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StatusNotificationResponse.json")))
+                   throw new Exception("Invalid response");
+           }
+           else if (i == 3)
+           {
+               if (!Utility.ValidateJSON(j, File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/HeartbeatResponse.json")))
+                   throw new Exception("Invalid response");
+           }
+       };
 
-                   if (j["status"] == null || j["status"].Value<string>() != "Accepted")
-                       throw new Exception("Invalid response");
-              
-               }
-               else if (i == 2)
-               {
-                 i++;
-                   if (!Utility.ValidateJSON(j, System.IO.File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StatusNotificationResponse.json")))
-                       throw new Exception("Invalid response");
-
-                  
-               }
-               else if (i == 3)
-               {
-                   if (!Utility.ValidateJSON(j, System.IO.File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/HeartbeatResponse.json")))
-                       throw new Exception("Invalid response");
-               }
-           };
-
-            ws.Send("[2,\"852a4cb2-0e20-46f8-bc29-c5ab3cb182c7\",\"BootNotification\",{\"chargePointVendor\":\"Radboud\",\"chargePointModel\":\"Fuzzer\"}]");
-            Thread.Sleep(1000);
-            ws.Send("[2,\"9b25cbb0-c016-41e7-baa0-e796a9565c11\",\"StatusNotification\",{\"connectorId\":1,\"errorCode\":\"NoError\",\"status\":\"Available\"}]");
-            Thread.Sleep(1000);
-            ws.Send("[2, \"a187bcd6-4042-4a82-b6d4-b4c55d2f2c8b\", \"Heartbeat\", {}]");
-            Thread.Sleep(1000);
-        }
+        ws.Send("[2,\"852a4cb2-0e20-46f8-bc29-c5ab3cb182c7\",\"BootNotification\",{\"chargePointVendor\":\"Radboud\",\"chargePointModel\":\"Fuzzer\"}]");
+        Thread.Sleep(1000);
+        ws.Send("[2,\"9b25cbb0-c016-41e7-baa0-e796a9565c11\",\"StatusNotification\",{\"connectorId\":1,\"errorCode\":\"NoError\",\"status\":\"Available\"}]");
+        Thread.Sleep(1000);
+        ws.Send("[2, \"a187bcd6-4042-4a82-b6d4-b4c55d2f2c8b\", \"Heartbeat\", {}]");
+        Thread.Sleep(1000);
+        
         return false;
     }
 }
--- Scenarios/TC_003_CSMS.cs
+++ Scenarios/TC_003_CSMS.cs
@@ -8,61 +8,63 @@ public class TC_003_CSMS : IScenario
 {
     public bool RunScenario(string url, string protocol)
     {
-         using (var ws = new WebSocket(url, protocol))
-        {
-            ws.Connect();
+        using var ws = new WebSocket(url, protocol);
 
-             int i = 1;
-             ws.OnMessage += (sender, e) =>
-            {
-                JArray a = JArray.Parse(e.Data);
-                JObject j = a[2] as JObject;
+        ws.Connect();
 
-                if (i == 1)
-                { 
-                    i++;
-                    if (!Utility.ValidateJSON(j, System.IO.File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StatusNotificationResponse.json")))
-                        throw new Exception("Invalid response");
+        int i = 1;
+        ws.OnMessage += (sender, e) =>
+       {
+           JArray a = JArray.Parse(e.Data);
+           JObject? j = a[2] as JObject;
 
-                    
-                }
-                else if (i == 2)
-                {     
-                    i++;  
-                    if (!Utility.ValidateJSON(j, System.IO.File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/AuthorizeResponse.json")))
-                        throw new Exception("Invalid response");
+           if (j == null)
+               return;
 
-                    if (j["idTagInfo"]["status"] == null || j["idTagInfo"]["status"].Value<string>() != "Accepted")
-                       throw new Exception("Invalid response");
+           if (i == 1)
+           {
+               i++;
+               if (!Utility.ValidateJSON(j, File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StatusNotificationResponse.json")))
+                   throw new Exception("Invalid response");
+           }
+           else if (i == 2)
+           {
+               i++;
+               if (!Utility.ValidateJSON(j, File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/AuthorizeResponse.json")))
+                   throw new Exception("Invalid response");
 
-                   
-                }
-                else if (i == 3)
-                {       
-                    i++;
-                    if (!Utility.ValidateJSON(j, System.IO.File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StartTransactionResponse.json")))
-                        throw new Exception("Invalid response");
+               if (j["idTagInfo"] == null ||
+                   j["idTagInfo"]["status"] == null ||
+                   j["idTagInfo"]["status"].Value<string>() != "Accepted")
+                   throw new Exception("Invalid response");
+           }
+           else if (i == 3)
+           {
+               i++;
+               if (!Utility.ValidateJSON(j, File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StartTransactionResponse.json")))
+                   throw new Exception("Invalid response");
 
-                        if (j["idTagInfo"]["status"] == null || j["idTagInfo"] ["status"].Value<string>() != "Accepted")
-                       throw new Exception("Invalid response");
+               if (j["idTagInfo"] == null ||
+                   j["idTagInfo"]["status"] == null ||
+                   j["idTagInfo"]["status"].Value<string>() != "Accepted")
+                   throw new Exception("Invalid response");
+           }
+           else if (i == 4)
+           {
+               if (!Utility.ValidateJSON(j, File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StatusNotificationResponse.json")))
+                   throw new Exception("Invalid response");
+           }
+       };
 
-                }
-                else if (i == 4)
-                {                   
-                    if (!Utility.ValidateJSON(j, System.IO.File.ReadAllText("/Users/bperry/projects/ocpp/v1.6_schemas/schemas/StatusNotificationResponse.json")))
-                        throw new Exception("Invalid response");
-                }
-            };
+        ws.Send("[2,\"9b25cbb0-c016-41e7-baa0-e796a9565c11\",\"StatusNotification\",{\"connectorId\":1,\"errorCode\":\"NoError\",\"status\":\"Preparing\"}]");
+        Thread.Sleep(1000);
+        ws.Send("[2,\"8d59bc8c-9884-4d64-82b5-3819d0c58b8a\",\"Authorize\",{\"idTag\":\"Authorize\"}]");
+        Thread.Sleep(1000);
+        ws.Send("[2,\"dddb2599-d678-4ff8-bf38-a230390a1200\",\"StartTransaction\",{\"connectorId\":1,\"idTag\":\"some id\",\"meterStart\":42,\"timestamp\":\"2017-10-27T19:10:11Z\"}]");
+        Thread.Sleep(1000);
+        ws.Send("[2,\"9b25cbb0-c016-41e7-baa0-e796a9565c11\",\"StatusNotification\",{\"connectorId\":1,\"errorCode\":\"NoError\",\"status\":\"Charging\"}]");
+        Thread.Sleep(1000);
 
-            ws.Send("[2,\"9b25cbb0-c016-41e7-baa0-e796a9565c11\",\"StatusNotification\",{\"connectorId\":1,\"errorCode\":\"NoError\",\"status\":\"Preparing\"}]");
-Thread.Sleep(1000);
-            ws.Send("[2,\"8d59bc8c-9884-4d64-82b5-3819d0c58b8a\",\"Authorize\",{\"idTag\":\"Authorize\"}]");
-Thread.Sleep(1000);
-            ws.Send("[2,\"dddb2599-d678-4ff8-bf38-a230390a1200\",\"StartTransaction\",{\"connectorId\":1,\"idTag\":\"some id\",\"meterStart\":42,\"timestamp\":\"2017-10-27T19:10:11Z\"}]");
-Thread.Sleep(1000);
-            ws.Send("[2,\"9b25cbb0-c016-41e7-baa0-e796a9565c11\",\"StatusNotification\",{\"connectorId\":1,\"errorCode\":\"NoError\",\"status\":\"Charging\"}]");
-Thread.Sleep(1000);
-        }
         return false;
     }
 }
