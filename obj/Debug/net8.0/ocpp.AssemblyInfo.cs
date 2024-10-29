73b0fbe4909ef1066cab4e4ba18176a7f5271e02
Brandon Perry <bperry@Brandons-MBP.attlocal.net>
2024-10-29 18:35:03 -0500

    refactor

--- obj/Debug/net8.0/ocpp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ocpp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ocpp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c227b7373fab78a8c495da25e9e52e497060169")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4556f7de7bea4dc0e4edb69d17e44669f340733e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ocpp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ocpp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
