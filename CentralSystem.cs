0b2ba2e68226f2ed66c4f786b66f8869b67fd06b
Brandon Perry <bperry.volatile@gmail.com>
2024-12-10 17:02:46 -0600

    new scenario

--- CentralSystem.cs
+++ CentralSystem.cs
@@ -5,25 +5,22 @@ namespace ocpp;
 
 public class CentralSystem : System
 {
-    List<IScenario> _scenarios;
     public CentralSystem(string url, string protocol)
     : base(url, protocol)
     {
-        _scenarios = new List<IScenario>();
-
-        _scenarios.Add(new TC_001_CSMS());
-        _scenarios.Add(new TC_003_CSMS());
-        _scenarios.Add(new TC_004_1_CSMS());
-        _scenarios.Add(new TC_004_2_CSMS());
-        _scenarios.Add(new TC_064_CSMS());
+        Scenarios =
+        [
+            new TC_001_CSMS(),
+            new TC_003_CSMS(),
+            new TC_004_1_CSMS(),
+            new TC_004_2_CSMS(),
+            new TC_064_CSMS(),
+        ];
     }
    
-
-    public List<IScenario> Scenarios { get { return _scenarios; }}
-
     public void RunScenarios()
     {
-        foreach (IScenario s in _scenarios)
+        foreach (IScenario s in Scenarios)
         {
             s.RunScenario(URL, Protocol);
         }
